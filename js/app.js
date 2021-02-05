96a1bfddd714c5707807be86653e259a332dca35
muh-dirga-f <muh.dirga.f@gmail.com>
2021-02-05 13:45:57 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+const app = new Framework7({
+    // App root element
+    el: '#app',
+    // App Name
+    name: 'Lelang',
+    // App id
+    id: 'com.myapp.lelang',
+    // Enable swipe panel
+    panel: {
+        swipe: true,
+    },
+    // Add default routes
+    routes: [
+        //admin-link
+        {
+            path: '/login-admin/',
+            componentUrl: 'pages/login-admin.html',
+        },
+        //user-link
+        {
+            path: '/registrasi/',
+            componentUrl: 'pages/registrasi.html',
+        },
+        {
+            path: '/home-user/',
+            componentUrl: 'pages/user/home.html',
+        },
+        {
+            path: '/lelang-user/',
+            componentUrl: 'pages/user/lelang.html',
+        },
+        {
+            path: '/lelang-terkini/',
+            componentUrl: 'pages/user/lelang-terkini.html',
+        },
+        {
+            path: '/lihat-lelang/',
+            componentUrl: 'pages/user/lihat-lelang.html',
+        },
+        {
+            path: '/daftar-barang/',
+            componentUrl: 'pages/user/daftar-barang.html',
+        },
+        {
+            path: '/login-user/',
+            componentUrl: 'pages/login-user.html',
+        },
+    ],
+});
+
+let store = app.createStore({
+    state: {
+        users: [],
+    },
+});
+
+let $ = Dom7;
+let mainView = app.views.create('.view-main');
+// app.views.main.router.navigate('/home-user/');
+
+$('.login-user').on('click', function () {
+    var formData = app.form.convertToData('#login-user');
+    if (formData.email != "" || formData.password != "") {
+        loginUser(formData).then(res => {
+            if (res.status) {
+                app.views.main.router.navigate('/home-user/');
+                store.state.users = [];
+                store.state.users.push(res.dataid,res.data);
+            } else {
+                app.dialog.alert(res.pesan);
+            }
+        });
+        // alert(JSON.stringify(formData));
+    } else {
+        app.dialog.alert("E-mail atau Password tidak boleh kosong");
+    }
+});
---
+++ js/firebase.js
@@ -0,0 +1,165 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAI9ksx6EPMQhULg07gG2KerLT36_NDC2Q",
+    authDomain: "test-89d4a.firebaseapp.com",
+    databaseURL: "https://test-89d4a.firebaseio.com",
+    projectId: "test-89d4a",
+    storageBucket: "test-89d4a.appspot.com",
+    messagingSenderId: "1075505772606",
+    appId: "1:1075505772606:web:c987f0826246c132e0396f",
+    measurementId: "G-5PFT1MB1PG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//? sistem login
+//* fungsi register
+async function registrasi(data) {
+    //cek email terdaftar
+    let status = true;
+    let pesan = "Registrasi berhasil. silahkan login!";
+    await firebase.database().ref('/users/').once('value').then((snapshot) => {
+        snapshot.forEach((v, i) => {
+            if (v.val().email == data.email) {
+                pesan = "E-mail telah terdaftar!";
+                status = false;
+            }
+            if (v.val().nik == data.nik) {
+                pesan = "NIK telah terdaftar!"
+                status = false;
+            }
+        });
+    });
+    if (status) {
+        firebase.database().ref('users').push().set(data);
+    }
+    return {
+        "status": status,
+        "pesan": pesan
+    };
+}
+
+//* fungsi login-user
+async function loginUser(data) {
+    let status = false;
+    let pesan = "E-mail belum terdaftar!";
+    let dataLogin = "";
+    let dataIdLogin = "";
+    await firebase.database().ref('/users/').once('value').then((snapshot) => {
+        snapshot.forEach((v, i) => {
+            if (v.val().email == data.email) {
+                if (v.val().password == data.password) {
+                    status = true;
+                    pesan = "login berhasil";
+                    dataLogin = v.val();
+                    dataIdLogin = v.key;
+                    return;
+                } else {
+                    pesan = "Password salah!"
+                    status = false;
+                }
+            }
+        });
+    });
+    return {
+        "status": status,
+        "pesan": pesan,
+        "data": dataLogin,
+        "dataid": dataIdLogin,
+    };
+}
+
+//* upload gambar
+async function uploadData(file, metadata, folderName) {
+    let pesan = "";
+    let url = "";
+    let extFile = "." + file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
+    // Push to child path.
+    await firebase.storage().ref().child(folderName + '/' + Date.now() + extFile).put(file, metadata).then(function (snapshot) {
+        pesan = 'Uploaded ' + snapshot.totalBytes + ' bytes.';
+        console.log('File metadata:', snapshot.metadata);
+        // Let's get a download URL for the file.
+        return url = new Promise((resolve) => {
+            snapshot.ref.getDownloadURL().then((dataUrl) => {
+                resolve(dataUrl);
+            })
+            
+        });
+    }).catch(function (error) {
+        pesan = 'Upload failed: ' + error;
+    });
+
+    return {
+        "url": url,
+        "pesan": pesan
+    };
+}
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!! //
+// TODO:
+//* 1. buat form daftar barang -> crud tambah barang untuk di lelang
+//tambah data
+async function tambahData(namaDB, data) {
+    let pesan = "";
+    let status = false;
+    await firebase.database().ref(namaDB).push().set(data, function (error) {
+        if (error) {
+            pesan = "Data tidak dapat disimpan." + error;
+        } else {
+            pesan = "Data berhasil tersimpan.";
+            status = true;
+        }
+    });
+    return {
+        "pesan": pesan,
+        "status": status
+    };
+}
+//hapus data
+async function hapusData(namaDB, id) {
+    let pesan = "";
+    await firebase.database().ref(namaDB + "/" + id).remove()
+        .then(function () {
+            pesan = "Data berhasil dihapus.";
+        })
+        .catch(function (error) {
+            pesan = "Data gagal dihapus." + error;
+        });
+    return {
+        "pesan": pesan
+    };
+}
+//select data
+async function getData(namaDB, id) {
+    let data = "";
+    await firebase.database().ref(namaDB + "/" + id).once('value').then((snapshot) => {
+        data = snapshot.val();
+    });
+    return {
+        "data": data
+    };
+}
+//update data
+async function updateData(namaDB, data, id) {
+    let pesan = "";
+    await firebase.database().ref(namaDB + "/" + id).update(data, function (error) {
+        if (error) {
+            pesan = "Data tidak dapat disimpan." + error;
+        } else {
+            pesan = "Data berhasil tersimpan.";
+        }
+    });
+    return {
+        "pesan": pesan
+    };
+}
+
+//* 2. fitur ikut lelang
+//cek lelang aktif dan nonaktif
+//if(waktu.server > waktu pada aplikasi) >> ubah status pada record jadi nonaktif else aktif
+//api timezone : http://api.timezonedb.com/v2.1/list-time-zone?key=J83H9QQ0IULO&format=json&country=ID&zone=Asia\/Makassar
+
+
+//* 3. fitur pemenang lelang
+//* 4. pembayaran
